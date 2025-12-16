5e6d1624c168bf62b4e62f8f0ee6f7d159138fa5
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2025-12-16 12:50:41 +0100

    add db sync before server start

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';
+import sequelize from './config/database.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3002;
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+async function start() {
+  try {
+    await sequelize.authenticate();
+    // eslint-disable-next-line no-console
+    console.log('DB connected');
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+start();
